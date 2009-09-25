f43c3f2db1850ae8050dd148890bfa6fbe0aa12c
nowa <nowazhu@gmail.com>
2009-09-25 10:45:02 +0800

    0.0.14

--- lib/top4r/client/suite.rb
+++ lib/top4r/client/suite.rb
@@ -8,7 +8,7 @@ class Top4R::Client
     u = u.nick if u.is_a?(Top4R::User)
     params = {:service_code => service_code}.merge(options).merge(:nick => u)
     response = http_connect {|conn| create_http_get_request(@@SUITE_METHODS[method], params)}
-    suites = Top4R::Suite.unmarshal(JSON.parse(response.body)["rsp"]["suite"])
+    suites = Top4R::Suite.unmarshal(JSON.parse(response.body)["rsp"]["suites"])
     suites.each {|suite| bless_model(suite); yield suite if block_given?}
     # puts "\nsuites: #{suites.inspect}"
     @total_results = JSON.parse(response.body)["rsp"]["totalResults"].to_i
