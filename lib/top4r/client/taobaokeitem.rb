3eddbd09a3da3eb47658d761f5e5c6a7bee81b8b
Nowa <nowazhu@gmail.com>
2010-06-17 11:05:09 +0800

    0.0.34

--- lib/top4r/client/taobaokeitem.rb
+++ lib/top4r/client/taobaokeitem.rb
@@ -1,9 +1,9 @@
 class Top4R::Client
   @@TAOBAOKEITEM_METHODS = {
-    :items_get => 'taobao.taobaoke.items.get'
+    :taobaoke_items_get => 'taobao.taobaoke.items.get'
   }
   
-  def taobaoke_items(q = nil, method = :items_get, options = {}, &block)
+  def taobaoke_items(q = nil, method = :taobaoke_items_get, options = {}, &block)
     valid_method(method, @@TAOBAOKEITEM_METHODS, :taobaoke_item)
     options = {:keyword => q}.merge(options) if q
     params = {:fields => Top4R::TaobaokeItem.fields}.merge(options)
