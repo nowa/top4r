202e2c69e34ed1c92198b76d82802b1cd4b33271
Jason Lee <huacnlee@gmail.com>
2011-07-07 22:26:50 +0800

    -

--- lib/top4r/client/item.rb
+++ lib/top4r/client/item.rb
@@ -1,9 +1,29 @@
 class Top4R::Client
   @@ITEM_METHODS = {
-    :onsale_list => 'taobao.items.onsale.get'
+    :onsale_list => 'taobao.items.onsale.get',
+    :inventory_list => 'taobao.items.inventory.get'
   }
   
   def items_onsale(q = nil, method = :onsale_list, options = {}, &block)
+    valid_method(method, @@ITEM_METHODS, :item)
+    options = {:q => q}.merge(options) if q
+    params = {:fields => Top4R::Item.fields}.merge(options)
+    response = http_connect {|conn| create_http_get_request(@@ITEM_METHODS[method], params)}
+    result = JSON.parse(response.body)[rsp(@@ITEM_METHODS[method])]
+    if result.is_a?(Hash) and result['items']
+      items = Top4R::Item.unmarshal(result["items"]["item"])
+      items.each {|item| bless_model(item); yield item if block_given?}
+      @total_results = result["total_results"].to_i
+    else
+      @total_results = 0
+      items = []
+    end
+    items
+  end
+  
+  # 得到当前会话用户库存中的商品列表
+  def items_inventory(q = nil, options = {}, &block)
+    method = :inventory_list
     valid_method(method, @@ITEM_METHODS, :item)
     options = {:q => q}.merge(options) if q
     params = {:fields => Top4R::Item.fields}.merge(options)
