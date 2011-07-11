a90003919c2370f28d5a9c5376a38030e525bf3f
Jason Lee <huacnlee@gmail.com>
2011-07-11 21:41:59 +0800

    获取单个宝贝接口加入

--- lib/top4r/client/item.rb
+++ lib/top4r/client/item.rb
@@ -2,7 +2,8 @@ class Top4R::Client
   @@ITEM_METHODS = {
     :onsale_list => 'taobao.items.onsale.get',
     :inventory_list => 'taobao.items.inventory.get',
-    :items_list => 'taobao.items.list.get'
+    :items_list => 'taobao.items.list.get',
+    :item => 'taobao.item.get'
   }
   
   def items_onsale(q = nil, method = :onsale_list, options = {}, &block)
@@ -60,5 +61,23 @@ class Top4R::Client
       items = []
     end
     items
+  end
+  
+  # 根据id列表获取单个宝贝
+  # 参数:
+  # id  商品数字id
+  def item(id)
+    method = :item
+    valid_method(method, @@ITEM_METHODS, :item)
+    options = {:num_iid => id}.merge({})
+    params = {:fields => Top4R::Item.fields}.merge(options)
+    response = http_connect {|conn| create_http_get_request(@@ITEM_METHODS[method], params)}
+    result = JSON.parse(response.body)[rsp(@@ITEM_METHODS[method])]
+    if result.is_a?(Hash) and result['item']
+      item = Top4R::Item.unmarshal(result["item"])
+    else
+      item = nil
+    end
+    item
   end
 end
